6da40a9966f9f8ee45eac713685bb74c9bbee383
Cassie <37855219+CodeF53@users.noreply.github.com>
2025-09-11 14:33:39 +0200

    add multi-system

--- flake.nix
+++ flake.nix
@@ -12,24 +12,62 @@
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: { nixosConfigurations.cassietop = nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    modules = [
-      ./hosts/cassietop/hardware-configuration.nix
-      ./configuration.nix
-      { imports = [ (import ./modules/enviroment-variables.nix { inherit inputs; }) ]; }
-      inputs.nix-flatpak.nixosModules.nix-flatpak
-      ./modules/flatpak.nix
-      ./modules/dev.nix
-      ./modules/gaming.nix
-
-      home-manager.nixosModules.home-manager { home-manager = {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+      homeConfig = host: {
         useGlobalPkgs = true;
         useUserPackages = true;
-        extraSpecialArgs = { inherit inputs; };
+        extraSpecialArgs = {
+          inherit inputs;
+          inherit host;
+        };
         users.cassie = import ./home.nix;
-        backupFileExtension = "bak";
-      }; }
-    ];
-  }; };
+      };
+    in
+    {
+      # laptop
+      nixosConfigurations.cassietop = nixpkgs.lib.nixosSystem rec {
+        inherit system;
+        specialArgs = {
+          host = "cassietop";
+          inherit inputs;
+        };
+        modules = [
+          ./hosts/cassietop/hardware-configuration.nix
+          ./configuration.nix
+          { imports = [ (import ./modules/enviroment-variables.nix { inherit inputs; }) ]; }
+          inputs.nix-flatpak.nixosModules.nix-flatpak
+          ./modules/flatpak.nix
+          ./modules/dev.nix
+          ./modules/gaming.nix
+          home-manager.nixosModules.home-manager
+          { home-manager = homeConfig specialArgs.host; }
+        ];
+      };
+      nixosConfigurations.cassiebox = nixpkgs.lib.nixosSystem rec {
+        inherit system;
+        specialArgs = {
+          host = "cassietop";
+          inherit inputs;
+        };
+        modules = [
+          ./hosts/cassiebox/hardware-configuration.nix
+          ./configuration.nix
+          { imports = [ (import ./modules/enviroment-variables.nix { inherit inputs; }) ]; }
+          inputs.nix-flatpak.nixosModules.nix-flatpak
+          ./modules/flatpak.nix
+          ./modules/dev.nix
+          ./modules/gaming.nix
+          home-manager.nixosModules.home-manager
+          { home-manager = homeConfig specialArgs.host; }
+        ];
+      };
+    };
 }
