5f8899977c92f87191075309d482ed1889be3ff5
Cassie <37855219+CodeF53@users.noreply.github.com>
2025-12-06 01:13:48 -0700

    +hypr config

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -5,9 +5,13 @@
   environment.systemPackages = with pkgs; [
     hyprlauncher
     hyprpaper
+    hyprtoolkit
     hyprshot
     playerctl
     jq
     hyprpolkitagent
   ];
+  home-manager.users.cassie = { config, ... }: {
+    xdg.configFile."hypr".source = config.lib.file.mkOutOfStoreSymlink /home/cassie/nixConfig/hypr;
+  };
 }
