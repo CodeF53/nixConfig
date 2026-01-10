c39a30c59b5919e2fe401ed2afb617761497e9a9
Cassie <37855219+CodeF53@users.noreply.github.com>
2026-01-10 02:25:06 -0700

    update nish
    mochie i swear to fucking god i will strangle you

--- homeManager/cli/fish.nix
+++ homeManager/cli/fish.nix
@@ -21,8 +21,15 @@ extras@{ pkgs, ... }:
     '';
     functions = {
       nish.body = ''
-        set -lx NIXPKGS_ALLOW_UNFREE 1
-        set -l pkgs "github:NixOS/nixpkgs/nixos-unstable#"$argv
+        argparse -N1 'm/master' -- $argv; or return 1
+
+        if set -q _flag_master
+            set -f pkgs "github:NixOS/nixpkgs/master#"$argv
+        else
+            set -f pkgs "nixpkgs#"$argv
+        end
+
+        set -fx NIXPKGS_ALLOW_UNFREE 1
         command nix shell --impure $pkgs
       '';
     };
