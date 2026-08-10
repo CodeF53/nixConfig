3822719841df93f9c280178dc22179b97aec073d
Cassie <37855219+CodeF53@users.noreply.github.com>
2026-08-10 09:39:07 -0600

    add some more binds, go insane setting mouse sensitivity

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -10,14 +10,24 @@ hl.config {
     master = { new_status = "master" },
 }
 
-; (function()
-    for _, name in pairs({
-        "logitech-g-pro--1",
-        "logitech-g-pro-wireless-gaming-mouse",
-    }) do hl.device({ name = name, sensitivity = -0.8 }) end
-
-    -- TODO: -0.1 for cassietop's trackpad
-end)()
+-- ; (function()
+--     for _, name in pairs({
+--         "logitech-g-pro--1",
+--         "logitech-g-pro-wireless-gaming-mouse",
+--     }) do hl.device({ name = name, sensitivity = -0.8 }) end
+--     -- TODO: -0.1 for cassietop's trackpad
+-- end)()
+hl.device({ name = "logitech-g-pro-", sensitivity = -0.8 })
+hl.device({ name = "logitech-g-pro--1", sensitivity = -0.8 })
+hl.device({ name = "logitech-g-pro--2", sensitivity = -0.8 })
+hl.device({ name = "logitech-g-pro-wireless-gaming-mouse", sensitivity = -0.8 })
+hl.device({ name = "logitech-g-pro-wireless-gaming-mouse-1", sensitivity = -0.8 })
+hl.device({ name = "logitech-g-pro-wireless-gaming-mouse-2", sensitivity = -0.8 })
+hl.config({
+    input = {
+        sensitivity = -0.8,
+    },
+})
 
 hl.on("hyprland.start", function()
     for _, cmd in pairs({
