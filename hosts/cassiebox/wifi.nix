56badf40368dc1599ccb9a6d326e65cc5a22fb49
Cassie <37855219+CodeF53@users.noreply.github.com>
2026-08-10 09:38:49 -0600

    8852au is broken

--- hosts/cassiebox/wifi.nix
+++ hosts/cassiebox/wifi.nix
@@ -2,6 +2,6 @@ args@{ pkgs, ... }:
 
 {
   hardware.usb-modeswitch.enable = true;
-  boot.extraModulePackages = [ args.config.boot.kernelPackages.rtl8852au ];
-  boot.initrd.kernelModules = [ "8852au" ];
+  # boot.extraModulePackages = [ args.config.boot.kernelPackages.rtl8852au ];
+  # boot.initrd.kernelModules = [ "8852au" ];
 }
