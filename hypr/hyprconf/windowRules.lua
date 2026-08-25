251f0fd920c0e4f8e2302c375dbbd144366659dc
Cassie <37855219+CodeF53@users.noreply.github.com>
2026-08-25 16:55:39 -0600

    remove flameshot windowrule

--- hypr/hyprconf/windowRules.lua
+++ hypr/hyprconf/windowRules.lua
@@ -14,16 +14,3 @@ hl.window_rule {
     match = { class = "^(zen-beta)$" },
     focus_on_activate = true,
 }
-
-hl.window_rule {
-    name = "flameshot",
-    match = { title = "flameshot" },
-
-    no_anim = true,
-    float = true,
-    pin = true,
-    move = { 0, 0 },
-    -- needs a forced scale attribute to work properly now with mixed scales
-    no_initial_focus = true,
-    monitor = 0, -- change to "0 silent" when https://github.com/hyprwm/Hyprland/discussions/15246 is closed
-}
