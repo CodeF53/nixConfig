2079274b3eaee2438c75161380d29eb4c0dbf0d0
Cassie <37855219+CodeF53@users.noreply.github.com>
2026-09-24 10:52:34 -0600

    update system, fix mic, update nvidia driver

--- hosts/cassiebox/audio.nix
+++ hosts/cassiebox/audio.nix
@@ -1,58 +1,45 @@
-{
-  services.pipewire.extraConfig.pipewire."99-combine-sink"."context.modules" = [
-    {
-      name = "libpipewire-module-combine-stream";
-      args = {
-        "combine.mode" = "sink";
-        "node.name" = "combined_output";
-        "node.description" = "mochie co-op gooning audio";
-        "combine.props" = {
-          "audio.position" = [ "FL" "FR" ];
-          "priority.driver" = 1500;
-          "priority.session" = 1500;
-        };
-        "stream.rules" = [
-          {
-            # array works as logical OR
-            matches = [
-              { "node.name" = "alsa_output.usb-SteelSeries_Arctis_Nova_Pro"; }
-              # rear audio output jack (dumbass dog NEEDS her "ATH-M50x"s EVERYWHERE)
-              { "node.nick" = "ALC897 Analog"; }
-            ];
-            actions = {
-              create-stream = {
-                "audio.position" = [ "FL" "FR" ];
-                "combine.audio.position" = [ "FL" "FR" ];
-              };
-            };
-          }
-        ];
-      };
-    }
-  ];
+{ pkgs, lib, ... }:
 
-  services.pipewire.wireplumber.extraConfig = {
-    "99-disable-useless-devices"."monitor.alsa.rules" = [
+lib.mkMerge [
+  { # elgato wave 3 fixes
+    # Disable USB autosuspend
+    services.udev.extraRules = ''
+      ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0070", ATTR{power/control}="on"
+    '';
+  
+    services.pipewire.wireplumber.extraConfig."50-elgato-wave3"."monitor.alsa.rules" = [
+      {
+        # Default to pro-audio profile
+        matches = [{ "device.name" = "~alsa_card.usb-Elgato_Systems_Elgato_Wave_3_*"; }];
+        actions.update-props."device.profile" = "pro-audio";
+      }
+      {
+        # Disable idle sleep timeout
+        matches = [{ "node.name" = "~alsa_input.usb-Elgato_Systems_Elgato_Wave_3_*"; }];
+        actions.update-props."session.suspend-timeout-seconds" = 0;
+      }
+    ];
+  }
+  {
+    services.pipewire.wireplumber.extraConfig."99-disable-useless-devices"."monitor.alsa.rules" = [
       {
         # disable audio devices I never use
         matches = [
           { "device.description" = "HDA NVidia"; } # GPU HDMI
           { "device.description" = "NexiGo N60 FHD Webcam"; }
           { "device.description" = "~Radeon High Definition Audio.*"; } # iGPU/AMD HDMI
-          # { "device.description" = "Family 17h/19h/1ah HD Audio Controller"; } # motherboard (3.5mm jacks)
+          { "device.description" = "Family 17h/19h/1ah HD Audio Controller"; } # motherboard (3.5mm jacks)
         ];
         actions.update-props."device.disabled" = true;
       }
       {
-        matches = [
-          {
-            # never use microphone as an output device
-            "node.description" = "Yeti Nano Analog Stereo";
-            "media.class" = "Audio/Sink";
-          }
-        ];
+        # microphone is not an output..
+        matches = [{
+          "node.name" = "~alsa_output.usb-Elgato_Systems_Elgato_Wave_3_*";
+          "media.class" = "Audio/Sink";
+        }];
         actions.update-props."node.disabled" = true;
       }
     ];
-  };
-}
+  }
+]
--- hosts/cassiebox/nvidia.nix
+++ hosts/cassiebox/nvidia.nix
@@ -11,26 +11,35 @@
     powerManagement.enable = true;
     powerManagement.finegrained = false;
     # package = config.boot.kernelPackages.nvidiaPackages.latest;
-    package =
-      (config.boot.kernelPackages.extend (
-        final: prev:
-        let
-          generic = args: final.callPackage (import "${inputs.nixpkgs}/pkgs/os-specific/linux/nvidia-x11/generic.nix" args) { };
-        in
-        {
-          nvidiaPackages.latest =
-            (generic {
-              version = "595.91.07";
-              sha256_64bit = "sha256-yiPIjdJLB6GRZE4eEc+3vN11NzBXSa9A+YABiwleYxM=";
-              sha256_aarch64 = "sha256-fqkN7ONFXtTeXyu2mQxorrk362Epxq3bz88hhKYQzwQ=";
-              openSha256 = "sha256-OB8Epd+qn/WywxsPiFpxEOAzlJqb6I1SyRoV3a8l71k=";
-              settingsSha256 = "sha256-QzT8Cw1luuZGP9DUje3HN/0ngiayqHURj+bqPsxlJ5w=";
-              persistencedSha256 = "sha256-3JQBaNmkwxvCXv9q8aHKas6VZM/JjLsuilC2t7ET0u0=";
-              patches = [];
-              patchesOpen = [ ./nvidia-bsb-dsc-fix.patch ];
-            });
-        }
-      )).nvidiaPackages.latest;
+    package = let
+        base = config.boot.kernelPackages.nvidiaPackages.latest;
+      in base.overrideAttrs (prev: {
+        passthru = prev.passthru // {
+          open = base.open.overrideAttrs (prevOpen: {
+            patches = (prevOpen.patches or [ ]) ++ [ ./nvidia-bsb-dsc-fix.patch ];
+          });
+        };
+      });
+    # package =
+    #   (config.boot.kernelPackages.extend (
+    #     final: prev:
+    #     let
+    #       generic = args: final.callPackage (import "${inputs.nixpkgs}/pkgs/os-specific/linux/nvidia-x11/generic.nix" args) { };
+    #     in
+    #     {
+    #       nvidiaPackages.latest =
+    #         (generic {
+    #           version = "595.104.02";
+    #           sha256_64bit = "sha256-5CHCAuTHn1jDx/MWG75xRU67PYiTb4ggWg4yfNBMWco=";
+    #           sha256_aarch64 = "sha256-PafStmwNMufeDp3VtpTGGCoW+53Gor/mieO1m1pI7gI=";
+    #           openSha256 = "sha256-FWk5ra2yjz8VAxAA8GXrSoeBj/XC1BKvsKsBKR09joE=";
+    #           settingsSha256 = "sha256-4Kxro6tvI5aX4nu2RspgyBsW+Jq3/VYjSAS5UGdzTCU=";
+    #           persistencedSha256 = "sha256-JsMLPqJuZwAtHngsQODMsmgO7F2tVkQ2arc7fYa2bwo=";
+    #           patches = [];
+    #           patchesOpen = [ ./nvidia-bsb-dsc-fix.patch ];
+    #         });
+    #     }
+    #   )).nvidiaPackages.latest;
     open = true;
     nvidiaSettings = true;
   };
